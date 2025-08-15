d42a4bba70d2fb3ecb854a5de534c760c449d090
BharathSunkarKumar <155315751+BharathSunkaraKumar@users.noreply.github.com>
2025-08-15 20:52:58 +0530

    completed

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-import express, { response } from "express";
-import { MONGO_URI, PORT } from "./config.js";
+import express from "express";
+// import { MONGO_URI, PORT } from "./config.js";
 import mongoose from "mongoose";
 import booksRouter from "./routes/booksRoutes.js";
 import cors from 'cors';
+import dotenv from 'dotenv'
 
+dotenv.config();
 const app = express();
+
+const MONGO_URI = process.env.MONGO_URI
+const PORT = process.env.PORT || 5500
 
 app.use(express.json())
 
